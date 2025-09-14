f2dff6b4fe804837f50ba10872d069e10b7a2278
Jarrod Carlson <jarrod.carlson@gmail.com>
2025-09-14 12:38:50 -0600

    Fix error in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
